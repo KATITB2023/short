87b7a6b9770fdbb333272d226c7777ce63199873
Gagas Praharsa Bahar <gagaspbahar@gmail.com>
2023-07-06 14:36:13 +0700

    add get all url

--- src/server/api/routers/url.ts
+++ src/server/api/routers/url.ts
@@ -3,43 +3,49 @@ import { env } from "~/env.mjs";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const urlRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
+  createURL: publicProcedure
+    .input(
+      z.object({
+        url: z.string().url(),
+        slug: z.string().optional(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      let slug: string;
+      if (!input.slug) {
+        slug = Math.random().toString(36).substring(2, 7);
+      } else {
+        slug = input.slug;
+      }
+      const existing = await ctx.prisma.url.findUnique({
+        where: {
+          slug,
+        },
+      });
+      if (existing) {
+        throw new Error("Slug already in use");
+      }
+      const url = await ctx.prisma.url.create({
+        data: {
+          url: input.url,
+          slug,
+        },
+      });
+      if (!url) {
+        throw new Error("Error creating URL");
+      }
       return {
-        greeting: `Hello ${input.text}`,
+        originalLink: url.url,
+        shortLink: env.HOST_URL + `${url.slug}`,
       };
     }),
-  createURL: publicProcedure.input(z.object({
-    url: z.string().url(),
-    slug: z.string().optional(),
-  })).query(async ({ ctx, input }) => {
-    let slug: string
-    if (!input.slug) {
-      slug = Math.random().toString(36).substring(2, 7);
-    } else {
-      slug = input.slug;
-    }
-    const existing = await ctx.prisma.url.findUnique({
-      where: {
-        slug,
+  getAllURLs: publicProcedure.query(async ({ ctx }) => {
+    return await ctx.prisma.url.findMany({
+      select: {
+        url: true,
+        slug: true,
+        clicks: true,
       },
     });
-    if (existing) {
-      throw new Error("Slug already in use");
-    }
-    const url = await ctx.prisma.url.create({
-      data: {
-        url: input.url,
-        slug,
-      },
-    });
-    if (!url) {
-      throw new Error("Error creating URL");
-    }
-    return {
-      originalLink: url.url,
-      shortLink: env.HOST_URL + `${url.slug}`,
-    };
   }),
 });
